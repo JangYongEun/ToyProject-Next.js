75e27bc2c5950a04d08181cf243c82079382fd6a
JangYongEun <92845228+JangYongEun@users.noreply.github.com>
2022-07-18 21:50:33 +0900

    Update Seo.tsx

--- src/component/Seo/Seo.tsx
+++ src/component/Seo/Seo.tsx
@@ -6,10 +6,8 @@ export default function Seo() {
       <NextSeo
         title="FIND&FICTURE"
         description="사진 속 정보찾기"
-        canonical="http://localhost:3000/"
         openGraph={{
           type: "website",
-          url: "http://localhost:3000/",
           title: "사진 속 정보찾기",
           description: "사진 속 정보찾기",
           images: [{ url: "https://t1.daumcdn.net/cfile/tistory/9991BA4A5B52C8931C" }],
